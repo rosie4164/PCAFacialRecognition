35d5b84afea81572b37c85551637470a84bcb4be
rosie4164 <96634391+rosie4164@users.noreply.github.com>
2022-09-14 21:24:07 -0400

    Add files via upload

---
+++ pca_cov.py
@@ -0,0 +1,246 @@
+'''pca_cov.py
+Performs principal component analysis using the covariance matrix approach
+Rosie Ingmann
+CS 251 Data Analysis Visualization
+Spring 2021
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+class PCA_COV:
+    '''
+    Perform and store principal component analysis results
+    '''
+
+    def __init__(self, data):
+        '''
+
+        Parameters:
+        -----------
+        data: pandas DataFrame. shape=(num_samps, num_vars)
+            Contains all the data samples and variables in a dataset.
+
+        (No changes should be needed)
+        '''
+        self.data = data
+
+        # vars: Python list. len(vars) = num_selected_vars
+        #   String variable names selected from the DataFrame to run PCA on.
+        #   num_selected_vars <= num_vars
+        self.vars = None
+
+        # A: ndarray. shape=(num_samps, num_selected_vars)
+        #   Matrix of data selected for PCA
+        self.A = None
+
+        # normalized: boolean.
+        #   Whether data matrix (A) is normalized by self.pca
+        self.normalized = None
+
+        # A_proj: ndarray. shape=(num_samps, num_pcs_to_keep)
+        #   Matrix of PCA projected data
+        self.A_proj = None
+
+        # e_vals: ndarray. shape=(num_pcs,)
+        #   Full set of eigenvalues (ordered large-to-small)
+        self.e_vals = None
+        # e_vecs: ndarray. shape=(num_selected_vars, num_pcs)
+        #   Full set of eigenvectors, corresponding to eigenvalues ordered large-to-small
+        self.e_vecs = None
+
+        # prop_var: Python list. len(prop_var) = num_pcs
+        #   Proportion variance accounted for by the PCs (ordered large-to-small)
+        self.prop_var = None
+
+        # cum_var: Python list. len(cum_var) = num_pcs
+        #   Cumulative proportion variance accounted for by the PCs (ordered large-to-small)
+        self.cum_var = None
+
+    def get_prop_var(self):
+        '''(No changes should be needed)'''
+        return self.prop_var
+
+    def get_cum_var(self):
+        '''(No changes should be needed)'''
+        return self.cum_var
+
+    def get_eigenvalues(self):
+        '''(No changes should be needed)'''
+        return self.e_vals
+
+    def get_eigenvectors(self):
+        '''(No changes should be needed)'''
+        return self.e_vecs
+
+    def covariance_matrix(self, data):
+        '''Computes the covariance matrix of `data`
+
+        Parameters:
+        -----------
+        data: ndarray. shape=(num_samps, num_vars)
+            `data` is NOT centered coming in, you should do that here.
+
+        Returns:
+        -----------
+        ndarray. shape=(num_vars, num_vars)
+            The covariance matrix of centered `data`
+
+        NOTE: You should do this wihout any loops
+        NOTE: np.cov is off-limits here — compute it from "scratch"!
+        '''
+        data_shape=np.shape(data)
+        data_means=np.mean(data, axis=0) #size 1xk
+        data_differences=data-data_means  #size nxk #it is centered now
+        matrix=data_differences.T@data_differences
+        matrix_final=matrix/(data_shape[0]-1)
+        return matrix_final
+
+    def compute_prop_var(self, e_vals):
+        '''Computes the proportion variance accounted for by the principal components (PCs).
+
+        Parameters:
+        -----------
+        e_vals: ndarray. shape=(num_pcs,)
+
+        Returns:
+        -----------
+        Python list. len = num_pcs
+            Proportion variance accounted for by the PCs
+        '''
+        return_list=[]
+        total=e_vals.sum()
+        for i in e_vals:
+            return_list.append(i/total)
+        return return_list
+
+    def compute_cum_var(self, prop_var):
+        '''Computes the cumulative variance accounted for by the principal components (PCs).
+
+        Parameters:
+        -----------
+        prop_var: Python list. len(prop_var) = num_pcs
+            Proportion variance accounted for by the PCs, ordered largest-to-smallest
+            [Output of self.compute_prop_var()]
+
+        Returns:
+        -----------
+        Python list. len = num_pcs
+            Cumulative variance accounted for by the PCs
+        '''
+        return_list=[]
+        current_total=0
+        for i in prop_var:
+            current_total=current_total+i
+            return_list.append(current_total)
+        return return_list
+
+    def pca(self, vars, normalize=False):
+        '''Performs PCA on the data variables `vars`
+
+        Parameters:
+        -----------
+        vars: Python list of strings. len(vars) = num_selected_vars
+            1+ variable names selected to perform PCA on.
+            Variable names must match those used in the `self.data` DataFrame.
+        normalize: boolean.
+            If True, normalize each data variable so that the values range from 0 to 1.
+
+        NOTE: Leverage other methods in this class as much as possible to do computations.
+
+        TODO:
+        - Select the relevant data (corresponding to `vars`) from the data pandas DataFrame
+        then convert to numpy ndarray for forthcoming calculations.
+        - If `normalize` is True, normalize the selected data so that each variable (column)
+        ranges from 0 to 1 (i.e. normalize based on the dynamic range of each variable).
+            - Before normalizing, create instance variables containing information that would be
+            needed to "undo" or reverse the normalization on the selected data.
+        - Make sure to compute everything needed to set all instance variables defined in constructor,
+        except for self.A_proj (this will happen later).
+        '''
+
+        self.A=self.data[vars].to_numpy()
+        self.vars=vars
+        self.min=np.min(self.A, axis=0)
+        self.max=np.max(self.A, axis=0)
+        if normalize==True:
+            self.normalized=True
+            self.A=(self.A-self.min)/(self.max-self.min)
+        cov_matrix=self.covariance_matrix(self.A)
+        self.e_vals, self.e_vecs=np.linalg.eig(cov_matrix)
+        self.prop_var=self.compute_prop_var(self.e_vals)
+        self.cum_var=self.compute_cum_var(self.prop_var)
+
+    def elbow_plot(self, num_pcs_to_keep=None):
+        '''Plots a curve of the cumulative variance accounted for by the top `num_pcs_to_keep` PCs.
+        x axis corresponds to top PCs included (large-to-small order)
+        y axis corresponds to proportion variance accounted for
+
+        Parameters:
+        -----------
+        num_pcs_to_keep: int. Show the variance accounted for by this many top PCs.
+            If num_pcs_to_keep is None, show variance accounted for by ALL the PCs (the default).
+
+        NOTE: Make plot markers at each point. Enlarge them so that they look obvious.
+        NOTE: Reminder to create useful x and y axis labels.
+        NOTE: Don't write plt.show() in this method
+        '''
+        if num_pcs_to_keep is not None:
+            plt.plot(range(1, num_pcs_to_keep+1, 1), self.cum_var[0:num_pcs_to_keep])
+            plt.title("# of Principal Components vs. Cumulative Variance")
+            plt.xlabel("# of PCs")
+            plt.ylabel("Cumulative Variance")
+        else:
+            plt.plot(range(1, len(self.e_vals)+1, 1), self.cum_var)
+            plt.title("# of Principal Components vs. Cumulative Variance")
+            plt.xlabel("# of PCs")
+            plt.ylabel("Cumulative Variance")
+
+    def pca_project(self, pcs_to_keep):
+        '''Project the data onto `pcs_to_keep` PCs (not necessarily contiguous)
+
+        Parameters:
+        -----------
+        pcs_to_keep: Python list of ints. len(pcs_to_keep) = num_pcs_to_keep
+            Project the data onto these PCs.
+            NOTE: This LIST contains indices of PCs to project the data onto, they are NOT necessarily
+            contiguous.
+            Example 1: [0, 2] would mean project on the 1st and 3rd largest PCs.
+            Example 2: [0, 1] would mean project on the two largest PCs.
+
+        Returns
+        -----------
+        pca_proj: ndarray. shape=(num_samps, num_pcs_to_keep).
+            e.g. if pcs_to_keep = [0, 1],
+            then pca_proj[:, 0] are x values, pca_proj[:, 1] are y values.
+
+        NOTE: This method should set the variable `self.A_proj`
+        '''
+        data_c=self.A-self.A.mean(axis=0)
+        pcs=self.e_vecs[:,pcs_to_keep]
+        self.A_proj=data_c@pcs
+        return self.A_proj
+
+    def pca_then_project_back(self, top_k):
+        '''Project the data into PCA space (on `top_k` PCs) then project it back to the data space
+
+        Parameters:
+        -----------
+        top_k: int. Project the data onto this many top PCs.
+
+        Returns:
+        -----------
+        ndarray. shape=(num_samps, num_selected_vars)
+
+        TODO:
+        - Project the data on the `top_k` PCs (assume PCA has already been performed).
+        - Project this PCA-transformed data back to the original data space
+        - If you normalized, remember to rescale the data projected back to the original data space.
+        '''
+        data_proj=self.pca_project(range(top_k))
+        projected_back=data_proj@self.e_vecs[:,range(top_k)].T+self.A.mean(axis=0)
+        if self.normalized==True:
+            projected_back=projected_back*(self.max-self.min)+self.min
+        return projected_back
+
